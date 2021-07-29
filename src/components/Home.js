c263fd54c1794f068b0e645648d4377765c616d1
rndimitrov <33346953+rndimitrov@users.noreply.github.com>
2021-07-29 18:18:00 +0100

    home page + navigation

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,13 @@
 import React from "react";
+import image from "../qyb.jpg";
 
 export default function Home() {
-    return <h1>Home</h1>
+    return (
+        <main>
+            <img src={ image } alt="Home page image" className="absolute object-cover w-full h-full" />
+            <section className="relative flex, justify-center min-h-screen pt-12 lg:pt-64 px-8" >
+                <h1 className="text-6xl text-green-100 fonmt-bold cursive leading-none lg:loading-snug home-name"> Oui, Amigo</h1>
+            </section>
+        </main>
+    )
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,30 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import { SocialIcon } from "react-social-icons";
 
 export default function Navbar() {
-    return <h1>Navbar</h1>
+    return (
+        <header className="bg-gray-500">
+            <div className="container mx-auto flex justify-between">
+                <nav className="flex">
+                    <NavLink to="/" exact activeClassName="text-white" className="inflex-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-green-600 text-4xl font-bold cursive tracking-widest" >
+                        Home
+                    </NavLink>
+                    <NavLink to="/post" activeClassName="text-red-100 bg-gray-400" className="inline-flex items-center rounded py-3 px-3 my-6 text-red-300 hover:text-green-600" >
+                        Blog posts
+                    </NavLink>
+                    <NavLink to="/project" activeClassName="text-red-100 bg-gray-400" className="inline-flex items-center rounded py-3 px-3 my-6 text-red-300 hover:text-green-600" >
+                        Projects
+                    </NavLink>
+                    <NavLink to="/about" activeClassName="text-red-100 bg-gray-400" className="inline-flex items-center rounded py-3 px-3 my-6 text-red-300 hover:text-green-600" >
+                        About Me
+                    </NavLink>
+                </nav>
+                <div className="inline-flex py-3 px-3 my-6" >
+                    <SocialIcon url="https://www.linkedin.com/in/rumendimitrov101/" className="mr-4" target="_blank" fgColor="#fff" style={{ height: 35, width: 35 }} />
+                    <SocialIcon url="https://github.com/rndimitrov" className="mr-4" target="_blank" fgColor="#fff" style={{ height: 35, width: 35 }} />
+                </div>
+            </div>
+        </header>
+    )
 }
